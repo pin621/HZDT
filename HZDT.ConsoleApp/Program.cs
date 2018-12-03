0b414b655c473c7c34ed5ed0d67d1ebd4311c6b8
pin621 <pin621@163.com>
2018-12-03 16:53:54 +0800

    Domain层代码完善

--- HZDT.ConsoleApp/Program.cs
+++ HZDT.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace HZDT.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
---
+++ HZDT.Respository.Interface/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HZDT.Respository.Interface
+{
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// 是否提交
+        /// </summary>
+        bool IsCommitted { get; set; }
+        /// <summary>
+        /// 提交
+        /// </summary>
+        /// <returns></returns>
+        int Commit();
+        /// <summary>
+        /// 回滚
+        /// </summary>
+        void Rollback();
+    }
+}
